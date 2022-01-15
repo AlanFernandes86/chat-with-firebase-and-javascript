2eeb8cc89809c5f64d67420792176dbd97831542
AlanFernandes <josefernandes.alan@gmail.com>
2022-01-15 13:54:14 -0300

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,78 @@
 import { database, analytics } from "./init-firebase.js";
-import { ref, set } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-database.js";
+import { ref, set, update, onValue, push} from "https://www.gstatic.com/firebasejs/9.6.3/firebase-database.js";
+import { Observable } from "./observable.js";
 
 class MainPage {
     constructor() {
+
         this._value = '';
+
         this.listeners();
+
+        this.list = new Observable({});
+        this.list.subscribe((newValue) => this.listData(newValue));
+
+        this._htmlUl = document.getElementById('list-test');
+        this._htmlUl.onclick = this.selectLine;
     }
     
     listeners() {
         document.getElementById('input')
-        .oninput = ({ target: { value : value} }) => this._value = value;
+        .oninput = ({ target: { value } }) => this._value = value;
 
         document.getElementById('btn')
-        .onclick = (_) => this.gravarDados();
-    }  
+        .onclick = (_) => this.upsertData();
 
-    gravarDados() {
-        console.log(this._value);
-        set(ref(database, 'Testes'), {
-            input: 'this._value',
-        })
+        onValue(ref(database, 'Testes'), (snapshot) => {
+            this.list.value = snapshot.val();
+        });
     }
 
+    htmlUlReset() {
+        this._htmlUl.innerHTML = '';
+    }
+    
+    listData(data) {
+        this.htmlUlReset();
+        Object.entries(data).forEach( (entry) => {
+            this._htmlUl
+            .appendChild(this.createLi(entry));
+        });
+    }
+
+    upsertData(key) {
+        update(ref(database, 'Testes'), {
+            [key || Date.now()] : this._value,
+        });
+    }
+
+    deleteData({ target: { id } }) {
+        console.log(id);
+        update(ref(database, 'Testes'), {
+            [id] : null,
+        });
+    }
+
+    selectLine({ target: { id } }) {
+
+    }
+        
+    createLi(data) {
+        const li = document.createElement('li');
+        
+        const span = document.createElement('span');
+        span.id = data[0];
+        span.className = 'material-icons';
+        span.innerText = 'delete';
+        span.onclick = this.deleteData;
+        
+        li.append(span);
+
+        li.id = data[0];
+        li.append(`${data[0]} --> ${data[1]}`);
+        
+        return li;
+    } 
 }
 
 export const mainPage = () => new MainPage();
